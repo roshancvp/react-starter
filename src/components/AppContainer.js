d6ddd9049e341315c1ff5c114fcbfe4a9b6c004f
Roshan Prabhakar <roshancvp@gmail.com>
2017-08-30 23:57:21 -0700

    Initial commit

---
+++ src/components/AppContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>)
+  }
+}
